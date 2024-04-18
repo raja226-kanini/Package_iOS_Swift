613fa137b8add110013da5262abd733208989546
raja226 <iosrajasekhar@gmail.com>
2024-04-18 15:26:42 +0530

    initial commit

---
+++ SamplePackageConsumed/Package_iOSSwift.framework/Headers/Package_iOSSwift.h
@@ -0,0 +1,18 @@
+//
+//  Package_iOSSwift.h
+//  Package_iOSSwift
+//
+//  Created by Administrator on 18/04/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Package_iOSSwift.
+FOUNDATION_EXPORT double Package_iOSSwiftVersionNumber;
+
+//! Project version string for Package_iOSSwift.
+FOUNDATION_EXPORT const unsigned char Package_iOSSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Package_iOSSwift/PublicHeader.h>
+
+
---
+++ SamplePackageConsumed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SamplePackageConsumed
+//
+//  Created by Administrator on 18/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
